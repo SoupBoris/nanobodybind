e05a230fedd9434f186a4f4b9aaf4813ba31f95f
soupboris <sebastian.berendson@utec.edu.pe>
2022-10-09 13:38:09 -0500

    Initial classes

---
+++ nanobodyAntigen/nanobodyMatch.py
@@ -0,0 +1,6 @@
+# open big DB of nanobodies
+
+# load up trained K-N and RF models
+
+# compare CDRs and pL and Hydrophobia using K-N and RF to find similar ones
+# compute binding probabilty for all datapoints
