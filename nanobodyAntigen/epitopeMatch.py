96ccd807f97b14b4bb2b13cfdacf7e80703c1d01
soupboris <sebastian.berendson@utec.edu.pe>
2022-10-09 14:23:39 -0500

    Notes on epitopematch struct

--- nanobodyAntigen/epitopeMatch.py
+++ nanobodyAntigen/epitopeMatch.py
@@ -1,3 +1,10 @@
 # compare string distance to existing epitopes in DB
 # compute list of top 3 most similar ones
 # return 1D array of binding nanobodies
+
+# load nanobodies-epitopes db into a pandas df
+# compare string distance to input value creating news column 'distance'
+
+# collect rows containing top 3 similar 'distance' entries (max or min)
+
+# return pandas df containing 3 rows of nanobody info
