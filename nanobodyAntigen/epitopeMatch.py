57f25caeea249c1cb62b9d5855810db6b2b5c4c5
soupboris <sebastian.berendson@utec.edu.pe>
2022-10-09 19:35:51 -0500

    Constructor and utilities

--- nanobodyAntigen/epitopeMatch.py
+++ nanobodyAntigen/epitopeMatch.py
@@ -4,3 +4,35 @@
 # collect rows containing top 3 similar 'distance' entries (min)
 
 # return pandas df containing N rows of nanobody info
+
+import this
+from pandas import DataFrame
+from pandas import read_csv
+
+
+class epitopeMatching():
+
+    # epitope argument, a string of the
+    # target epitope
+    epitope = ''
+
+    # database of matching epitopes to
+    # nanobodies that bind together
+    epitopeDatabase = DataFrame()
+
+
+    # --- Constructor funct ---
+    # Creates epitope variable as well as
+    # loading up database of matching
+    # epitopes to nanobodies
+    def  __init__(epitopeInput):
+        epitope = epitopeInput
+        epitopeDatabase = read_csv('databases\realCDRS.csv')
+
+    # --- Set input funct ---
+    # Utility function to set input manually
+    def setInputEpitope(epitopeInput):
+        epitope = epitopeInput
+
+    
+
