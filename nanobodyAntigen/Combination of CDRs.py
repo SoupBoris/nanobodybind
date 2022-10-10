6916b6d635d061c5a639fce57fd9c6076e3bc1cb
soupboris <sebastian.berendson@utec.edu.pe>
2022-10-10 05:18:59 -0500

    fix: general functionality

--- nanobodyAntigen/Combination of CDRs.py
+++ nanobodyAntigen/Combination of CDRs.py
@@ -2,11 +2,13 @@ from CDR1 import finalmat
 from CDR2 import finalmat2
 from CDR3 import finalmat3
 
-CombinationsofCDRs = [None] * 120000000000000
+CombinationsofCDRs = []
 
 for ay in range(5000):
     for aydios in range(7680):
         for aydiosmio in range(3125000):
-            CombinationsofCDRs[aydiosmio+(aydios*50000)+(aydiosmio*24000000000)]=finalmat[ay]+finalmat2[aydios]+finalmat3[aydiosmio]
+            CombinationsofCDRs.append(f'{finalmat[ay]}{finalmat2[aydios]}{finalmat3[aydiosmio]}')
+            print(aydiosmio)
+    print(ay)
 
 print(CombinationsofCDRs)
